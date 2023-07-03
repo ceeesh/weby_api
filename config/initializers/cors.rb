43d818a2a6a0d45f92ddf6a68d4f638a87972b34
ceeesh <ceejaymalacas17@gmail.com>
2023-07-03 12:03:32 +0800

    url update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "https://weby-beta.vercel.app"
     
-    # https://weby-beta.vercel.app
+    # http://localhost:5173 
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
